e2ed936f566b91a5c75ef15458f471bbdbe4db00
Jorick van Hees <jorick.vanhees@incentro.com>
2022-02-27 16:01:36 +0100

    Fix globalstyles

--- .storybook/themeDecorator.tsx
+++ .storybook/themeDecorator.tsx
@@ -3,10 +3,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './../src/styles/theme';
+import GlobalStyle from './../src/styles/GlobalStyle';
 
 export const ThemeDecorator = Story => {
     return (
         <ThemeProvider theme={theme}>
+            <GlobalStyle />
             <Story />
         </ThemeProvider>
     );
--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,19 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
 
 import GlobalStyle from './styles/GlobalStyle';
-import { Button } from './components/button/Button';
+import { Button } from './components/ui/button/Button';
 import { Heading } from './components/typography/heading/Heading';
 
 const App: FC = () => {
     return (
         <ThemeProvider theme={theme}>
-            <GlobalStyle>
-                <div className="App">
-                    <Heading level={1} as="h1">
-                        Heading 1
-                    </Heading>
-                    <Button primary={true}>Hello</Button>
-                </div>
-            </GlobalStyle>
+            <GlobalStyle />
+            <div className="App">
+                <Heading level={1} as="h1">
+                    Heading 1
+                </Heading>
+                <Button primary={true}>Hello</Button>
+            </div>
         </ThemeProvider>
     );
 };
